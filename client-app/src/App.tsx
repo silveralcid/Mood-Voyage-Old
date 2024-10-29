9f292d8adf7fa6cd332076f30657ae7c7c6861e8
Silver Alcid <silveralcid@gmail.com>
2024-10-29 12:07:10 -0400

    Clean up app.tsx.

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,6 +1,4 @@
-import { useEffect, useState } from 'react';
 import './App.css';
-import axios from 'axios';
 import AssessmentForm, { AssessmentData } from './components/AssessmentForm';
 import { createAssessment } from './services/assessmentService';
 
