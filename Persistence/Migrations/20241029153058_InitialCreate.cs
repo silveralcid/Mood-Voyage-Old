bc7a0ae91d5e283c961294a5c93b1da31b4e95e0
Silver Alcid <silveralcid@gmail.com>
2024-10-29 11:32:31 -0400

    Assessments database setup.

---
+++ Persistence/Migrations/20241029153058_InitialCreate.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Assessments",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    UserId = table.Column<string>(type: "TEXT", nullable: true),
+                    LivelihoodAvgRating = table.Column<byte>(type: "INTEGER", nullable: false),
+                    ConnectionAvgRating = table.Column<byte>(type: "INTEGER", nullable: false),
+                    EsteemAvgRating = table.Column<byte>(type: "INTEGER", nullable: false),
+                    AutonomyAvgRating = table.Column<byte>(type: "INTEGER", nullable: false),
+                    PurposeAvgRating = table.Column<byte>(type: "INTEGER", nullable: false),
+                    ActualizationAvgRating = table.Column<byte>(type: "INTEGER", nullable: false),
+                    Date = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Assessments", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Assessments");
+        }
+    }
+}
